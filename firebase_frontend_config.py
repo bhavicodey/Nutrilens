615abcf5da4fadbe06d2a9090e50fe34cda4b308
Bhavishyaa Vignesh <67924047+bhavicodey@users.noreply.github.com>
2025-10-25 20:36:48 -0700

    Add files via upload

---
+++ firebase_frontend_config.py
@@ -0,0 +1,10 @@
+firebase_frontend_config = {
+    "apiKey": "AIzaSyB3qT_TpUggThiIKLJ78OaRuPCQWq5bWuw",
+    "authDomain": "nutrilens-5cc34.firebaseapp.com",
+    "projectId": "nutrilens-5cc34",
+    "storageBucket": "nutrilens-5cc34.firebasestorage.app",
+    "messagingSenderId": "894107369115",
+    "appId": "1:894107369115:web:a41318433c1b41fee3903e",
+    "measurementId": "G-E694D7J7W2",
+    "databaseURL": ""
+}
